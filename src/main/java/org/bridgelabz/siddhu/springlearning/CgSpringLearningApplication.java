48cceec6a13d840f28128d9976adbf541178a1f0
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-21 15:43:17 +0530

    🎉 Initial commit

---
+++ src/main/java/org/bridgelabz/siddhu/springlearning/CgSpringLearningApplication.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.springlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgSpringLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgSpringLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bridgelabz/siddhu/springlearning/CgSpringLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bridgelabz.siddhu.springlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgSpringLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
